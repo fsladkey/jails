885620680847bde1b446abbaea6bcd919af5231f
Fred Sladkey <fsladkey@gmail.com>
2016-08-17 11:04:18 -0400

    Add basic repl

---
+++ config/console.js
@@ -0,0 +1,15 @@
+const Models = require('../orm/models')
+const User = require('../app/models/user')
+const glob = require( 'glob' )
+const path = require( 'path' );
+const repl = require("repl");
+
+require('require-all')(__dirname + '/../app/models');
+
+const newRepl = repl.start("node::jails> ");
+
+Models._load(() => {
+  Models.models.forEach(model => {
+    newRepl.context[model.name] = model;
+  })
+})
